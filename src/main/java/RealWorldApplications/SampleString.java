234eda965bb80c8c91770ed4d6382e7de43b5b63
PriyamPratyushSahu <priyampratyushsahu3232@gmail.com>
2025-05-29 09:47:26 +0530

    aa

---
+++ src/main/java/RealWorldApplications/SampleString.java
@@ -0,0 +1,15 @@
+package RealWorldApplications;
+
+public class SampleString {
+    public static void main(String[] args) {
+        String ss = "";
+        String str1[] = ss.split("\n");
+        int i = 1;
+        for(String s : str1){
+            System.out.println(s);
+
+
+
+        }
+    }
+}
