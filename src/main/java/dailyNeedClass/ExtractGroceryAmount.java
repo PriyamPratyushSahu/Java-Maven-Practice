28dbefe7eaac60ffbea219c13d5b117254f16655
PriyamPratyushSahu <priyampratyushsahu3232@gmail.com>
2026-04-27 09:59:31 +0530

    Update

--- src/main/java/dailyNeedClass/ExtractGroceryAmount.java
+++ src/main/java/dailyNeedClass/ExtractGroceryAmount.java
@@ -2,23 +2,30 @@ package dailyNeedClass;
 
 public class ExtractGroceryAmount {
     public static void main(String[] args) {
-        String groceryString = "Cucumber(4) = ₹ 19.78\n" +
-                "Tomato(4) = ₹ 10.07\n" +
-                "Onion(8/835gm) = ₹ 15.03\n" +
-                "Dhaniya = ₹ 8\n" +
-                "Pudina = ₹ 8\n";
+        String groceryString = "";
         String eachItem[] = groceryString.split("\n");
         String totalFormula = "= ";
+        Double totalPrice = 0.0;
         System.out.println("Individual Price print");
         for(String i :eachItem){
+            // Spliting string into two parts - Item name, Price
             String priceSplit[] = i.split("₹ ");
+
+            //Extracting the bracket content
+            int leftBracketPos = priceSplit[0].indexOf('(');
+            int rightBracketPos = priceSplit[0].indexOf(')');
+            String insideBracketValue = priceSplit[0].substring(leftBracketPos,(rightBracketPos+1));
+            System.out.println(insideBracketValue +" / ₹ " + priceSplit[1]);
+            totalPrice += Double.parseDouble(priceSplit[1]);
+            //Preparing a string of total formula
             if(totalFormula.compareTo("= ") == 0)
             totalFormula = totalFormula.concat(priceSplit[1]);
             else
                 totalFormula = totalFormula.concat(" + "+priceSplit[1]);
-            System.out.println(priceSplit[1]);
         }
-        System.out.println("\n\n\nFormula Print");
+        System.out.println(totalPrice);
+        System.out.println("\nFormula Print");
         System.out.println(totalFormula);
+
     }
 }
